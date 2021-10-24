58dd81102a194a1295d431992b8dd99b9c81e731
Troudge <nlockwood617@gmail.com>
2021-10-23 23:08:56 -0600

    Updated readme and uncommented assignment 2 main. Updated run file and added some code to manully compute weights for gradient at bottom of assignment 2 main

--- MainScripts/Assignment2Main.py
+++ MainScripts/Assignment2Main.py
@@ -1,13 +1,10 @@
-from Decision_Trees.Id3 import Id3Tree
 from Decision_Trees import Id3
-from Decision_Trees.tree import Node
 from Linear_Regression import RegressionLearners
-import networkx as nx
-import matplotlib.pyplot as plt
 from Ensemble_Learning import EnsembleLearners
+import numpy as np
 
 test_small_data = [[1, -1, 2, 1],
-                   [1,  1, 3, 4],
+                   [1, 1, 3, 4],
                    [-1, 1, 0, -1],
                    [1, 2, -4, -2],
                    [3, -1, -1, 0]]
@@ -59,25 +56,26 @@ atr2 = {(0, 'age'), (1, 'job'), (2, 'married'), (3, 'education'), (4, 'default')
         (12, 'campaign'), (13, 'pdays'), (14, 'previous'), (15, 'poutcome')}
 atr3 = {(0, 'Cement'), (1, 'Slag'), (2, 'Fly ash'), (3, 'Water'), (4, 'SP'), (5, 'Course Aggr.'), (6, 'Fine Aggr.')}
 # tests of the adaboost algorithm
-#for i in range(1, 100):
-#    learner = EnsembleLearners.EnsembleLearner(dataset_bank, atr2, 16)
-#    forest = learner.adaboost(i)
-#    print(EnsembleLearners.run_forest_on_set(forest, Id3.convert_numeric_set_to_boolean(dataset_bank, atr2), atr2, 16))
+for i in range(1, 100):
+    learner = EnsembleLearners.EnsembleLearner(dataset_bank, atr2, 16)
+    forest = learner.adaboost(i)
+    print(EnsembleLearners.run_forest_on_set(forest, Id3.convert_numeric_set_to_boolean(dataset_bank, atr2), atr2, 16))
 
 
 # bank_forest = bank_learner.adaboost(500)
 # print(EnsembleLearners.run_forest_on_set(bank_forest, dataset_bank, atr2, 16))
 
-# for i in range(1, 100):
-#    bank_learner = EnsembleLearners.EnsembleLearner(dataset_bank, atr2, 16)
-#    bank_forest_bagged = bank_learner.bagged_trees(i, 2500)
-#    print(EnsembleLearners.run_forest_on_set(bank_forest_bagged, Id3.convert_numeric_set_to_boolean(dataset_bank, atr2), atr2, 16))
+for i in range(1, 100):
+    bank_learner = EnsembleLearners.EnsembleLearner(dataset_bank, atr2, 16)
+    bank_forest_bagged = bank_learner.bagged_trees(i, 2500)
+    print(EnsembleLearners.run_forest_on_set(bank_forest_bagged, Id3.convert_numeric_set_to_boolean(dataset_bank, atr2)
+                                             , atr2, 16))
 
-#for i in range(1, 100):
-#    bank_learner = EnsembleLearners.EnsembleLearner(dataset_bank, atr2, 16)
-#    bank_random = bank_learner.random_forest(i, 6, 2500)
-#    print(EnsembleLearners.run_forest_on_set(bank_random, Id3.convert_numeric_set_to_boolean(dataset_bank_test, atr2), atr2,
-#                                             16))
+for i in range(1, 100):
+    bank_learner = EnsembleLearners.EnsembleLearner(dataset_bank, atr2, 16)
+    bank_random = bank_learner.random_forest(i, 6, 2500)
+    print(EnsembleLearners.run_forest_on_set(bank_random, Id3.convert_numeric_set_to_boolean(dataset_bank_test, atr2),
+                                             atr2, 16))
 
 # An example of a decision tree stump
 # decision_tree = Id3Tree(dataset2, atr, 4, 'information_gain')
@@ -85,16 +83,24 @@ atr3 = {(0, 'Cement'), (1, 'Slag'), (2, 'Fly ash'), (3, 'Water'), (4, 'SP'), (5,
 #         with_labels=True, arrows=True)
 # plt.show()
 
-# concrete_learner = RegressionLearners.RegressionLearners(dataset_concrete, atr3)
-# min_weights = concrete_learner.batch_gradient_descent(0.03169, 5000, 0.000001)
-# print(RegressionLearners.run_weight_vec_cost_function(min_weights, dataset_concrete_test, 7))
-# print("min Weights: ", min_weights)
-
-# concrete_learner = RegressionLearners.RegressionLearners(dataset_concrete, atr3)
-# min_weights = concrete_learner.stochastic_gradient_descent(0.0125, 200, 0.000001)
-# print(RegressionLearners.run_weight_vec_cost_function(min_weights, dataset_concrete_test, 7))
-# print("min Weights: ", min_weights)
-atr4 = {(0, 'x_1'), (1, 'x_2'), (2, 'x_3')}
-l = RegressionLearners.RegressionLearners(test_small_data, atr4)
-min_weights = l.batch_gradient_descent(0.1, 5, 0.000001)
+concrete_learner1 = RegressionLearners.RegressionLearners(dataset_concrete, atr3)
+min_weights = concrete_learner1.batch_gradient_descent(0.03169, 5000, 0.000001)
+print(RegressionLearners.run_weight_vec_cost_function(min_weights, dataset_concrete_test, 7))
 print("min Weights: ", min_weights)
+
+concrete_learner2 = RegressionLearners.RegressionLearners(dataset_concrete, atr3)
+min_weights = concrete_learner2.stochastic_gradient_descent(0.0125, 200, 0.000001)
+print(RegressionLearners.run_weight_vec_cost_function(min_weights, dataset_concrete_test, 7))
+print("min Weights: ", min_weights)
+
+numeric_data = []
+for i in range(len(dataset_concrete)):
+    numeric_data.append(list(float(x) for x in dataset_concrete[i]))
+X = np.array(list((row[:7] for row in numeric_data)))
+Y = np.array(list((row[7] for row in numeric_data)))
+YT = Y.reshape((len(Y), 1))
+XT = X.transpose()
+inverse = np.linalg.inv(np.matmul(X, XT))
+left = np.matmul(inverse, X)
+result = np.dot(left.transpose(), YT)
+print(result)
--- README.md
+++ README.md
@@ -1,2 +1,23 @@
 # Machine-Learning-Final-Library
 This is a machine learning library developed by Nathan Lockwood forCS5350/6350 in University of Utah
+
+
+Some notes on the classes that have been created:
+all classes have a basic initializer that must be declared first. This stores the dataset and attribute information.
+An example of this is shown below:
+
+`` bank_learner = EnsembleLearners.EnsembleLearner(dataset_bank, atr2, 16)``
+
+The input attributes  marked "atr2" is a set of tuples inn the form (column index, attribute name), which is an int and a string. 
+After declaring a new instance of the learner, you can call any of the machine learning algorithms associcated with it.
+
+The Regression Learners class contains the gradient descent algorithms
+
+The Ensemble Learners class contains the Boosting and bagging methods
+
+The Id3 class contains Id3 and the stumps method (the generate id3 stump is used for the some of the ensemble learners).
+
+Additionally, there are some other methods not part of the classes in each module/file. the most useful of which start with run, and take in a dataset and some other parameters to run the learned algorithms.
+
+Lastly any of the algorithms that use trees utilize my node class. you can call print on a node to print out the tree structure to the console. 
+Additionally, you can call the to_graph method to convert a node tree to a network x graph, and then you can plot it as you see fit.

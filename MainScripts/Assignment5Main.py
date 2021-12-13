6711dbce15d86ed13533ec49bbe0ede99995fadf
Troudge <nlockwood617@gmail.com>
2021-12-12 23:52:35 -0700

    Added a functional version of pytorch network, and added to implementation of custom network

--- MainScripts/Assignment5Main.py
+++ MainScripts/Assignment5Main.py
@@ -1,7 +1,29 @@
 import numpy as np
-from Neural_Networks.BackPropNetwork import SGDNeuralNetwork, initialize_weights_normal, initialize_weights_zero
+from Neural_Networks.BackPropNetwork import SGDNeuralNetwork, initialize_weights_normal, initialize_weights_zero, \
+    CustomNeuralNetwork, generate_weights
 from torch import optim
 from torch import nn
+import torch
+
+
+def init_weight_func():
+    return np.random.normal()
+
+
+def test_model(dataset, network, labels):
+    accuracy = 0
+    for idx, row in enumerate(dataset):
+        result = network(torch.tensor(row, dtype=torch.float)).item()
+        # print(result)
+        r = 0
+        if result >= 1:
+            r = 1
+        else:
+            r = 0
+        if r == labels[idx]:
+            accuracy += 1
+    print('Error', 1 - accuracy / len(dataset))
+
 
 dataset_banknote = []
 with open('../Data/bank-note/train.csv', 'r') as file:
@@ -19,13 +41,30 @@ with open('../Data/bank-note/test.csv', 'r') as file:
 
 train_labels = np.array(dataset_banknote)[:, -1]
 train_inputs = np.array(dataset_banknote)[:, :-1]
-model = SGDNeuralNetwork(5)
+
+test_labels = np.array(dataset_banknote_test)[:, -1]
+test_inputs = np.array(dataset_banknote_test)[:, :-1]
+
+# section for my  implementation of the network. I was having trouble with this part, so it doesn't run without error
+# the code for it is in backpropnetwork.py
+# my_net = CustomNeuralNetwork(3, init_weight_func, 5, nn.Sigmoid)
+# my_net.forward(train_inputs[1])
+# my_net.backpropagation_compute_gradient(train_labels[1])
+
+# a neural network using pytorch
+model = SGDNeuralNetwork(15)
 model.apply(initialize_weights_normal)
 
-optimizer = optim.Adam(model.parameters(), lr=0.0001)
+optimizer = optim.Adam(model.parameters(), lr=0.001)
 for idx, row in enumerate(train_inputs):
+    expected = np.array([train_labels[idx]])
     optimizer.zero_grad()
-    output = model(row)
-    loss = nn.MSELoss(output, train_labels[idx])
-    loss.backward()
+    inp = model(torch.tensor(row, dtype=torch.float))
+    loss = nn.MSELoss()
+    output = loss(inp, torch.tensor(expected, dtype=torch.float))
+    output.backward()
     optimizer.step()
+
+# test model accuracy
+test_model(train_inputs, model, train_labels)
+test_model(test_inputs, model, test_labels)
--- Neural_Networks/BackPropNetwork.py
+++ Neural_Networks/BackPropNetwork.py
@@ -3,9 +3,7 @@ from torch import nn
 import numpy as np
 
 
-def backpropagation(network, input_example):
-    network.layers
-    pass
+# computes the partial derivatives assuming loss function
 
 
 # init weights should be a function that returns a single initialized weight
@@ -13,34 +11,57 @@ def generate_weights(init_weight_func, amount):
     weights = []
     for i in range(amount):
         weights.append(init_weight_func())
-    return weights
+    return np.array(weights)
 
 
 class CustomNeuralNetwork:
-    def __init__(self, num_layers, init_weights, layer_width) -> None:
+    def __init__(self, num_layers, init_weights, layer_width, activation_func) -> None:
         self.network = []
+        self.output = 0
         self.num_layers = num_layers
-        self.weights = init_weights()
+        self.biases = [1] * num_layers
         for i in range(num_layers):
             layer = []
             for j in range(layer_width):
-                n = Neuron(generate_weights(init_weights, layer_width))
+                n = Neuron(generate_weights(init_weights, layer_width), activation_func)
                 layer.append(n)
             self.network.append(layer)
 
-    def forward(self):
+    def forward(self, input_X):
+        for i in range(self.num_layers):
+            layer = self.network[i]
+            for neuron in layer:
+                if neuron.prev is None:
+                    neuron.input = input_X
+                else:
+                    neuron.input = neuron.prev.output
+                neuron.output = neuron.activation_func(np.dot(neuron.input, neuron.weights))
+
+    def backpropagation_compute_gradient(self, expected):
+        grads = []
+        for i, layer in reversed(list(enumerate(self.network))):
+            for j, neuron in enumerate(layer):
+                print(i, j)
+                neuron.grad = (self.output - expected) * neuron.weights
+
         pass
 
 
 class Neuron:
-    def __init__(self, weights):
+    def __init__(self, weights, activation_func):
         self.weights = weights
+        self.input = None
+        self.output = None
+        self.grad = 0
+        self.activation_func = activation_func
+        self.prev = None
+        self.next = None
 
 
 def initialize_weights_normal(m):
     if isinstance(m, nn.Linear):
         if m.weight is not None:
-            nn.init.normal(m.weight.data)
+            nn.init.normal_(m.weight.data)
         if m.bias is not None:
             nn.init.constant_(m.bias.data, 1)
 
@@ -56,16 +77,15 @@ def initialize_weights_zero(m):
 class SGDNeuralNetwork(nn.Module):
     def __init__(self, w) -> None:
         super().__init__()
-        self.layer1 = nn.Linear(w, w)
+        self.layer1 = nn.Linear(4, w)
         self.layer2 = nn.Linear(w, w)
         self.layer3 = nn.Linear(w, 1)
+        self.relu = nn.ReLU()
+        self.tanh = nn.Tanh()
 
-        self.network = nn.Sequential(
-            self.layer1(),
-            self.layer2(),
-            self.layer3()
-        )
-
-    def forward(self, x) -> torch.Tensor:
-        return self.network(x)
-        pass
+    def forward(self, inp) -> torch.Tensor:
+        x = self.tanh(self.layer1(inp))
+        for i in range(9):
+            x = self.tanh(self.layer2(x))
+        x = self.layer3(x)
+        return x
